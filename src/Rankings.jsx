7ebc49ecd2b8c2dd059a8f19768e93848a1909db
dsky62 <mrdwalker64@hotmail.com>
2026-03-28 13:16:30 -0700

    Update classYears to start with 2026

---
+++ src/Rankings.jsx
@@ -0,0 +1,7 @@
+const classYears = [
+    { id: 1, name: 'Class of 2026', desc: '2026 Rankings' },
+    { id: 2, name: 'Class of 2027', desc: '2027 Rankings' },
+    { id: 3, name: 'Class of 2028', desc: '2028 Rankings' }
+];
+
+export default classYears;
